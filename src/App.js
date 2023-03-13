50c6a7a9b3a5fd131cd3ac5879a502ed9ac42a97
ClaireL21 <102630261+ClaireL21@users.noreply.github.com>
2023-03-13 16:40:24 +0000

    Clicking feature works -  button text changes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,33 @@
 import React from 'react';
 
 const buttonStyle = {
-  margin: '10px 0px',
-  outerWidth: '500px',
-  
+  margin: '10px 0px'  
 };
-
-// const Button = ({label, handleClick}) => (
-//   <button
-//     className = "btn btn-default"
-//     style = {buttonStyle}
-//     onClick = {handleClick}
-//     >
-//       {label}
-//       </button>
-// );
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {isOn: true};
+    this.handleClick = this.handleClick.bind(this);
   }
 
   //handler goes here
-
+  handleClick() {
+    this.setState(prevState => ({isOn: !prevState.isOn}));
+  };
+  
   render() {
     return (
       <div>
         <p>Greetings!</p>
         <p> 
-          <button
+          { <button
             className = "btn btn-default"
             style = {buttonStyle} 
-          > Click Me</button> </p>
+            onClick = {this.handleClick} 
+            >{this.state.isOn ? "Click Me" : "Thanks"}
+            </button> }
+            </p>
       </div>
     );
   }
