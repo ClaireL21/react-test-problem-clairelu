1af39bb8a2ff0e3ff21a4cbfd7579318b133d788
ClaireL21 <102630261+ClaireL21@users.noreply.github.com>
2023-03-13 22:45:13 +0000

    Storybook for button

--- src/App.stories.js
+++ src/App.stories.js
@@ -1,2 +1,31 @@
 // Stories for testing `App` go here
 // See https://storybook.js.org/docs/react/writing-stories/introduction#how-to-write-stories
+
+
+import React from 'react';
+import App from './App';
+
+export default {
+    title: "Button",
+    text: "Click Me"
+};
+
+//export const ClickButton = () => <App> </App>;
+
+const Template = args => <App {...args} />;
+
+export const State1 = Template.bind({});
+State1.args = {
+    app: {
+        title: "Button in State 1",
+        text: "Click Me"
+    }
+}
+
+export const State2 = Template.bind({});
+State2.args = {
+    app: {
+        title: "Button in State 2",
+        text: "Thanks"
+    }
+}
