51ed60617f03a47920afecb07f3ba2d1306341a5
ClaireL21 <102630261+ClaireL21@users.noreply.github.com>
2023-03-16 16:20:18 +0000

    Added additional button states - for storybook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const buttonStyle = {
   margin: '10px 0px'  
@@ -32,3 +33,65 @@ export default class App extends React.Component {
     );
   }
 }
+
+/**
+ * Additional Buttons that allow the user
+ * to test functionality and appearance properties
+ * in storybook
+ */
+
+// Allows user to input different labels as button text
+const ButtonText = (props) => {
+  const {
+    text
+  } = props;
+  return (
+    <div>
+      <p>Greetings!</p>
+        <p> 
+          <button
+          className = "btn btn-default"
+            style = {buttonStyle} 
+            //onClick = {this.handleClick} 
+            >{text}
+          </button>
+        </p>
+    </div>
+  );
+}; 
+
+ButtonText.propTypes = {
+  text: PropTypes.string
+};
+ButtonText.defaultProps = {
+  text: "default"
+};
+export {ButtonText};
+
+// Allows user to test functionality of button labels 
+// based on user input for button state
+const ButtonChangeState = (props) => {
+  const {
+    buttonState
+  } = props;
+  return (
+    <div>
+      <p>Greetings!</p>
+        <p> 
+          <button
+          className = "btn btn-default"
+            style = {buttonStyle} 
+            >{buttonState ? "Click Me" : "Thanks"}
+          </button>
+        </p>
+    </div>
+  );
+}; 
+
+ButtonChangeState.propTypes = {
+  buttonState: PropTypes.bool
+};
+ButtonChangeState.defaultProps = {
+  buttonState: true
+};
+export {ButtonChangeState};
--- src/App.stories.js
+++ src/App.stories.js
@@ -1,35 +1,42 @@
 // Stories for testing `App` go here
 // See https://storybook.js.org/docs/react/writing-stories/introduction#how-to-write-stories
 
-
 import React from 'react';
 import App from './App';
+import {ButtonText} from './App';
+import {ButtonChangeState} from './App';
 
 export default {
-    title: "Button",
-    component: App
+    title: "Button"
 };
 
-//export const ClickButton = () => <App> </App>;
-
-const Template = args => <App {...args} />;
-
+const Template = (args) => <App {...args} />;
 export const ButtonState = Template.bind({});
 ButtonState.args = {
-    app: {
-        title: "Button",
+    button: {
+        title: "Default Button",
         component: App
     }
 }
 
-// export const State2 = Template.bind({});
-// State2.args = {
-//     app: {
-//         title: "Button in State 2",
-//         text: "Thanks"
-//     }
-// }
+// Template to allow user to input different labels 
+// as button text in storybook
+const Template2 = (args) => <ButtonText {...args} />;
+export const ButtonState2 = Template2.bind({});
+ButtonState2.args = {
+    button: {
+        title: "Changeable Text Button"
+    },
+    text: "button text here"
+};
 
-// export const Primary = () => {
-
-// }
+// Template to allow user to test functionality of button labels 
+// based on user input for button state in storybook
+const Template3 = (args) => <ButtonChangeState {...args} />;
+export const ButtonState3 = Template3.bind({});
+ButtonState3.args = {
+    button: {
+        title: "Changeable State Button"
+    },
+    buttonState: false
+};
